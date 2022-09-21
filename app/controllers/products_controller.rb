a0b14e285e7486ca1890479cdbdf642eb528d79f
Ruben Gomez <rubncal.9501@gmail.com>
2022-09-20 22:15:34 -0500

    Create new product and form

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create product_params
+    if @product.save
+        redirect_to products_path
+    else
+        render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit :code, :name, :description, :main_stotage, :current_storage, :price
+  end
+end

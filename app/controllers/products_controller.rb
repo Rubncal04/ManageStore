15269d809972411ad2d97ee7f1eb55d6acd82f23
Ruben Gomez <rubncal.9501@gmail.com>
2022-09-29 19:10:10 -0500

    Implement edit, update and show method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
+  before_action :set_image, only: [:edit, :update]
+
   def index
-    @products = Product.all()
+    @products = Product.all
   end
 
   def new
@@ -16,7 +18,26 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update product_params
+      redirect_to @product
+    else
+      flash.alert = "Something was wrong. Please, verify the information"
+    end
+  end
+
+  def show
+    
+  end
+
   private
+
+  def set_image
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit :code, :name, :description, :main_storage, :current_storage, :price
